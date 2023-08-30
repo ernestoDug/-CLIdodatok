042220f6173575f6cba54a7ae323dd1492360772
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2023-08-31 01:03:49 +0300

    6

--- contacts.js
+++ contacts.js
@@ -1,35 +1,59 @@
 const path = require("path");
 const fs = require("fs").promises;
+const {nanoid} = require('nanoid');
+// npm i nanoid@3.3.4
 
-const contactsPath = path.join("db", "contacts.json");
 
-const getParseContactsPath = async (path) => {
-  const data = await fs.readFile(path);
-  return JSON.parse(data);
-};
+const contactsPath = path.join(__dirname, 'db/contacts.json');
 
+
+
+// 1
 const listContacts = async () => {
   try {
-    const parseData = await getParseContactsPath(contactsPath);
-    console.table(parseData);
+    const data = await fs.readFile(contactsPath);
+        return JSON.parse(data);
   } catch (error) {
-    console.log(error);
+    console.log("cannot read contacts");
   }
 };
 
+// 2
 const getContactById = async (contactId) => {
   try {
-    const parseData = await getParseContactsPath(contactsPath);
-    const findContact = parseData.find((item) => item.id === contactId);
-    console.table(findContact);
+    const dataForId = await listContacts();
+
+    const findContact = dataForId.find((item) => item.id === contactId);
+    return  findContact || null;
+      } catch (error) {
+    console.log("cannot read id");
+  }
+};
+
+// 3
+const addContact = async (name, email, phone) => {
+  try {
+    const dataForAdd = await listContacts();
+    
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+
+    const data = [...dataForAdd, newContact];
+    // console.table("bfb");
+    const recordCont =  await fs.writeFile(contactsPath, JSON.stringify(data));
+    return  recordCont;
   } catch (error) {
-    console.log(error);
+    console.log("cannot write new contact");
   }
 };
 
 const removeContact = async (contactId) => {
   try {
-    const parseData = await getParseContactsPath(contactsPath);
+    const parseData = await listContacts(contactsPath);
     const deleteContact = parseData.filter((item) => item.id !== contactId);
     console.table(deleteContact);
     return await fs.writeFile(contactsPath, JSON.stringify(deleteContact));
@@ -38,22 +62,8 @@ const removeContact = async (contactId) => {
   }
 };
 
-const addContact = async (name, email, phone) => {
-  try {
-    const parseData = await getParseContactsPath(contactsPath);
-    const newContact = {
-      id: `${parseData.length + 1}`,
-      name,
-      email,
-      phone,
-    };
-    const data = [...parseData, newContact];
-    console.table(data);
-    return await fs.writeFile(contactsPath, JSON.stringify(data));
-  } catch (error) {
-    console.log(error);
-  }
-};
+// listContacts();
+
 
 module.exports = {
   listContacts,
--- index.js
+++ index.js
@@ -1,48 +1,57 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
 const { Command } = require("commander");
 
-const {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact,
-  } = require("./contacts");
-  
-  
-  const program = new Command();
-  program
-    .option("-a, --action <type>", "choose action")
-    .option("-i, --id <type>", "user id")
-    .option("-n, --name <type>", "user name")
-    .option("-e, --email <type>", "user email")
-    .option("-p, --phone <type>", "user phone");
-  
-  program.parse(process.argv);
-  
-  const argv = program.opts();
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-  console.log(555)
-  
-  function invokeAction({ action, id, name, email, phone }) {
-    switch (action) {
-      case "list":
-        listContacts();
-        break;
-  
-      case "get":
-        getContactById(id);
-        break;
-  
-      case "add":
-        addContact(name, email, phone);
-        break;
-  
-      case "remove":
-        removeContact(id);
-        break;
-  
-      default:
-        console.warn("\x1B[31m Unknown action type!");
-    }
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, name, id,  email, phone }) {
+
+  switch (action) {
+    case "list":
+     const listCont =  await   listContacts();
+      console.log(listCont);
+     break;
+
+    case "get":
+      const findIdCont = await  getContactById(id);
+      console.log(findIdCont);
+     break;
+
+
+    case "add":
+    const newCont =   await  addContact(name, email, phone);
+    return console.log(newCont);
+      break;
+
+    case "remove":
+      await    removeContact(id);
+      break;
+
+    default:
+      // console.warn("\x1B[31m Unknown2 action type!");
   }
-  
-  invokeAction(argv);
+}
+
+invokeAction(argv);
+
+
+// invokeAction({action: 'list'});  
+// invokeAction({action: 'get', id: "qdggE76Jtbfd9eWJHrssH" }); 
+// invokeAction({action: 'add', email: "abracadabra@bred.net",phone: 456546546 }); 
+
+
